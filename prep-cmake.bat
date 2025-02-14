8b4218c8d9679d18c2777fb4c5ed1535839e6603
Elias Bachaalany <elias.bachaalany@gmail.com>
2025-02-14 08:15:31 -0800

    build for ida 9.x only

--- prep-cmake.bat
+++ prep-cmake.bat
@@ -7,20 +7,13 @@ setlocal
 if not defined IDASDK (
     echo IDASDK environment variable not set.
     echo Also make sure ida-cmake is installed in IDASDK.
-    echo See: https://github.com/0xeb/ida-cmake
+    echo See: https://github.com/allthingsida/ida-cmake
     goto :eof
 )
 
-if not exist %IDASDK%\include\idax\xkernwin.hpp (
-    echo IDAX framework not properly installed in the IDA SDK folder.
-    echo See: https://github.com/0xeb/idax
-    goto :eof
-)
+if not exist build cmake -B build -S . -A x64 -DEA64=YES
 
-if not exist build cmake -A x64 -B build -S .
-if not exist build64 cmake -B build64 -S . -A x64 -DEA64=YES
-
-if "%1"=="build" cmake --build build --config Release && cmake --build build64 --config Release
+if "%1"=="build" cmake --build build --config Release
 
 echo.
 echo All done!
